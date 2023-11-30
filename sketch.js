a122f0bd28e6a9bebb649045a6de6841fba49c3e
its-a-me-Bryan <147672560+its-a-me-Bryan@users.noreply.github.com>
2023-11-29 18:43:07 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var trex
+var trexRunning
+var trexCollide
+
+function preload() {
+
+}
+
+function setup() {
+  createCanvas(600, 200);
+  trex = createSprite(50,180,20,50)
+}
+
+function draw() {
+  
+}
